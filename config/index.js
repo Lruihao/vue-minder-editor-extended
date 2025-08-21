5998a46bdba1a39fb7533c03c8af8a3b18bb64f5
Cell <1024@lruihao.cn>
2025-08-21 17:02:23 +0800

    Chore: 隐藏 sourceMap

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 var path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   build: {
@@ -7,12 +8,30 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    productionSourceMap: true,
+    productionSourceMap: false,
     productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
     bundleAnalyzerReport: process.env.npm_config_report,
-    devtool: "eval"
-    //devtool: 'cheap-module-eval-source-map'
+    devtool: "eval",
+    optimization: {
+      minimizer: [
+        // 解决视讯的安全扫描，webpack 源码泄漏
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: false, // 移除所有 console
+              drop_debugger: true, // 移除 debugger
+              // pure_funcs: ['console.log'] // 移除特定函数
+            },
+            output: {
+              comments: false // 移除所有注释
+            },
+            mangle: true // 混淆变量名
+          },
+          extractComments: false // 不提取注释到单独文件
+        })
+      ]
+    },
   },
   dev: {
     env: require('./dev.env'),
